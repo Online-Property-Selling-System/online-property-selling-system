3ab35b60d48fc632c974db9fdf2af2f221d6a968
Radhe5886 <ambhoreradheshyam@gmail.com>
2026-02-03 15:33:37 +0530

    Rent.jsx

---
+++ frontend/online-property-selling-system/src/pages/Rent.jsx
@@ -0,0 +1,16 @@
+import PropertyListing from "./PropertyListing";
+
+function Rent() {
+  return (
+    <>
+      {/* Rent Hero UI */}
+      <PropertyListing
+        type="RENT"
+        title="Properties for Rent"
+        subtitle="Find affordable homes & apartments for rent"
+      />
+    </>
+  );
+}
+
+export default Rent;
